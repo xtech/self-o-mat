6197f66ee469e668d7528b8ca44e9979fc064c64
Clemens Elflein <clemens1@familie-elflein.de>
2019-06-25 09:46:41 +0200

    added opencv info target

---
+++ src/main_opencv_info.cpp
@@ -0,0 +1,10 @@
+//
+// Created by clemens on 25.06.19.
+//
+
+#include <opencv2/opencv.hpp>
+#include <iostream>
+
+int main(int argc, char *argv[]) {
+    std::cout << cv::getBuildInformation() << std::endl;
+}
